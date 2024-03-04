ca3ad8f80d53fe9fc4c9fac85b3802a47e5a0ac2
Hajin74 <yoonmij9974@naver.com>
2024-03-04 17:16:07 +0900

    feat: 주소지 등록 api 연결

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,17 +2,50 @@ INSERT INTO User (user_id, password, username, email, phone_number, birthday)
 VALUES ('john_doe', 'password123', 'John Doe', 'john@example.com', '010-1234-5678', '1990-01-01');
 
 INSERT INTO User (user_id, password, username, email, phone_number, birthday)
-VALUES ('samadasoo123', 'water123', 'samda', 'samda@gmail.com', '010-3333-3333', '1991-11-31');
+VALUES ('alice_smith', 'password456', 'Alice Smith', 'alice@example.com', '010-9876-5432', '1995-05-15');
+
+INSERT INTO User (user_id, password, username, email, phone_number, birthday)
+VALUES ('김철수', 'securepwd123', '김철수', 'chulsoo@example.com', '010-9876-5432', '1985-03-25');
+
 
 
 INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
 VALUES (1, '우리집', '010-1004-5005', '서울시 강남구 역삼동', '123-456번지', 12345, 0, 1);
 
 INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
-VALUES (2, '회사', '010-3341-6445', '경기도 성남시 분당구 ', '333-333번지', 53815, 0, 1);
+VALUES (1, '회사', '02-333-4444', '서울특별시 서초구', '789-101번지', '54321', 0, 0);
 
 INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
-VALUES (1, '여기여기', '010-1004-5005', '천안', '123-456번지', 12345, 0, 0);
+VALUES (1, '이사간 주소', '010-5678-9012', '대전광역시 서구 계룡로 890번길', '빌라 404호', '45678', 0, 0);
 
 INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
-VALUES (1, '저기저기기', '010-1004-5005', '천안', '123-456번지', 12345, 0, 0);
+VALUES (2, '집', '010-1111-2222', '서울시 종로구 인사동', '789-123번지', 54321, 0, 1);
+
+INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
+VALUES (2, '친구 집', '010-5555-6666', '경기도 수원시', '111-222번지', '67890', 0, 0);
+
+INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
+VALUES (3, '부모님 집', '010-7777-8888', '인천광역시 남구', '333-444번지', '98765', 0, 0);
+
+INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
+VALUES (3, '휴가지', '010-9999-0000', '제주특별자치도 제주시', '555-666번지', '54321', 0, 0);
+
+INSERT INTO Address (user_id, address_name, phone_number, main_address, sub_address, zip_code, is_deleted, is_default)
+VALUES (3, '여행지 주소', '010-2345-6789', '부산광역시 해운대구 해운대해변로 123번길', '리조트 303호', '54321', 0, 0);
+
+
+INSERT INTO Book (title)
+VALUES ('귀신들의 땅');
+
+INSERT INTO Book (title)
+VALUES ('남아 있는 나날');
+
+INSERT INTO Book (title)
+VALUES ('모던 자바 인 액션');
+
+INSERT INTO Book (title)
+VALUES ('객체지향의 사실과 오해');
+
+INSERT INTO Book (title)
+VALUES ('불안의 서');
+
--- src/main/resources/static/address/addressList.js
+++ src/main/resources/static/address/addressList.js
@@ -1,14 +1,45 @@
 document.addEventListener('DOMContentLoaded', getAddressList);
 
+let defaultAddress;
+
+function getDefaultAddress(defaultAddress) {
+    const defaultAddressWrap = document.querySelector('.default-address-wrap');
+    defaultAddressWrap.innerHTML = '';
+
+    const defaultAddressHtml = `
+        <p>배송주소록</p>
+        <div class="address-info">
+            <span>기본배송지</span> |
+            <span>${defaultAddress['addressName']}</span><br>
+            <span>${defaultAddress['phoneNumber']}</span><br>
+            [<span>${defaultAddress['zipCode']}</span>]
+            <span>${defaultAddress['mainAddress']}</span>
+        </div>
+    `
+
+    defaultAddressWrap.insertAdjacentHTML('beforeend', defaultAddressHtml);
+}
+
 async function getAddressList() {
     const response = await fetch("/myroom/address");
     const addressList = await response.json();
 
     const addressListWrap = document.querySelector('.address-list-wrap');
-    addressListWrap.innerHTML = '<button type="submit">+ 새 주소록 등록</button>';
+    addressListWrap.innerHTML = '';
 
     addressList.forEach(address => {
-        const isDefaultSpan = address['default'] ? '| <span>기본배송지</span>' : '';
+        let isDefaultSpan;
+        let defaultAddressBtn;
+
+        if (address['default'] === true) {
+            defaultAddress = address;
+            isDefaultSpan = '| <span>기본배송지</span>';
+            defaultAddressBtn = '';
+        } else {
+            isDefaultSpan = '';
+            defaultAddressBtn = '<button class="set-default-address-btn" type="submit">기본 배송지로 설정</button>';
+        }
+
         const addressItemHtml = `
             <div class="address-item-wrap">
                 <div class="address-info">
@@ -18,9 +49,48 @@ async function getAddressList() {
                     [<span>${address['zipCode']}</span>]
                     <span>${address['mainAddress']}</span>
                 </div>
-                <button type="submit">수정</button>
+                <div class="address-item-button-wrap">
+                    ${defaultAddressBtn}
+                    <button class="edit-address-btn" type="submit">수정</button>
+                </div>
             </div>
         `
         addressListWrap.insertAdjacentHTML('beforeend', addressItemHtml);
     })
+
+    getDefaultAddress(defaultAddress);
 }
+
+async function addAddress(address){
+    const response = await fetch("/myroom/address", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(address)
+    })
+
+    const createdAddress = await response.json();
+    console.log(createdAddress.addressName);
+    location.reload();
+}
+
+const modal = document.querySelector(".modal");
+const submitBtn = document.querySelector("#submitAddress");
+const addAddressButton = document.querySelector(".add-address-btn");
+addAddressButton.addEventListener('click', () => {
+    modal.style.display = "block";
+});
+
+submitBtn.addEventListener('click',  () => {
+    const address = {
+        addressName: document.getElementById("addressName").value,
+        phoneNumber: document.getElementById("phoneNumber").value,
+        mainAddress: document.getElementById("mainAddress").value,
+        subAddress: document.getElementById("subAddress").value,
+        zipCode: document.getElementById("zipCode").value
+    }
+
+    addAddress(address);
+    modal.style.display = "none";
+});
