483204d98c2b9e1188e385610bcb961c2d10261e
Hajin74 <yoonmij9974@naver.com>
2024-03-04 12:20:45 +0900

    feat: address 예외 처리 핸들러

--- src/main/java/eliceproject/bookstore/address/AddressController.java
+++ src/main/java/eliceproject/bookstore/address/AddressController.java
@@ -1,8 +1,10 @@
 package eliceproject.bookstore.address;
 
-import eliceproject.bookstore.common.ApiResponse;
+import eliceproject.bookstore.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,71 +12,60 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/myroom/member/address")
+@RequestMapping("/myroom/address")
 @RequiredArgsConstructor
 public class AddressController {
 
+    private final AddressRepository addressRepository;
     private final AddressService addressService;
 
     /* 주소지 등록 */
     @PostMapping
-    public ApiResponse<Address> addAddress(@RequestBody Address address) {
-        try {
-            return ApiResponse.success(addressService.create(address), "주소지 등록에 성공했습니다.");
-        } catch (Exception e) {
-            log.error("주소지 등록에 실패했습니다.", e);
-            return ApiResponse.fail(500, null, "주소지 등록에 실패했습니다.");
+    public ResponseEntity<Address> addAddress(@RequestBody Address address) throws Exception {
+        Address createdAddress = addressService.create(address);
+        if (createdAddress == null) {
+            throw new Exception("주소지 등록에 실패했습니다.");
         }
+        return new ResponseEntity<>(createdAddress, HttpStatus.OK);
     }
 
     /* 주소지 전체 조회 */
     @GetMapping
-    public ApiResponse<List<Address>> getAllAddress() {
-        try {
-            List<Address> addressList = addressService.findAll();
-            return ApiResponse.success(addressList, "주소지 전체 조회에 성공했습니다.");
-        } catch (Exception e) {
-            log.error("주소지 전체 조회에 실패했습니다.", e);
-            return ApiResponse.fail(500, null, "주소지 전체 조회에 실패했습니다.");
-        }
+    public ResponseEntity<List<Address>> getAllAddress() {
+        List<Address> addressList = addressService.findAll();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
     }
 
-    /* 기본 주소지 설정 및 나머지는 기본 주소지 해제 */
+
+    /* 기본 주소지 설정 */
     @PutMapping("/{addressId}/default")
-    public ApiResponse<Address> setDefaultAddress(@PathVariable Long addressId) {
-        try {
-            Long userId = 1L;
-            addressService.setDefault(userId, addressId);
-            Address defaultAddress = addressService.findById(addressId);
-            return ApiResponse.success(defaultAddress, "기본 주소시 설정에 성공했습니다.");
-        } catch (Exception e) {
-            log.error("기본 주소지 설정에 실패했습니다.", e);
-            return ApiResponse.fail(500, null, "기본 주소지 설정에 실패했습니다.");
+    public ResponseEntity<Address> setDefaultAddress(@PathVariable Long addressId) throws Exception {
+        Long userId = 1L;
+        addressService.setDefault(userId, addressId);
+        Address defaultAddress = addressService.findById(addressId);
+        if (defaultAddress == null) {
+            throw new Exception("기본 주소지 설정에 실패했습니다.");
         }
+        return new ResponseEntity<>(defaultAddress, HttpStatus.OK);
     }
 
     /* 주소시 수정 */
     @PatchMapping("/{addressId}")
-    public ApiResponse<Address> updateAddress(@PathVariable Long addressId, @RequestBody AddressDTO addressDTO) {
-        try {
-            Address updateAddress = addressService.update(addressId, addressDTO);
-            return ApiResponse.success(updateAddress, "주소지 수정에 성공했습니다.");
-        } catch (Exception e) {
-            log.error("주소시 수정에 실패했습니다.", e);
-            return ApiResponse.fail(500, null, "주소시 수정에 실패했습니다.");
+    public ResponseEntity<Address> updateAddress(@PathVariable Long addressId, @RequestBody AddressDTO addressDTO) throws Exception {
+        Address updateAddress = addressService.update(addressId, addressDTO);
+        if (updateAddress == null) {
+            throw new Exception("주소지 수정에 실패했습니다.");
         }
+        return new ResponseEntity<>(updateAddress, HttpStatus.OK);
     }
 
     /* 주소시 삭제 */
     @DeleteMapping("/{addressId}")
-    public ApiResponse<Object> deleteAddress(@PathVariable Long addressId) {
-        try {
-            addressService.delete(addressId);
-            return ApiResponse.success(null, "주소지 삭제에 성공했습니다.");
-        } catch (Exception e) {
-            log.error("주소시 삭제에 실패했습니다.", e);
-            return ApiResponse.fail(500, null, "주소시 삭제에 실패했습니다.");
-        }
+    public ResponseEntity<Object> deleteAddress(@PathVariable Long addressId) throws ResourceNotFoundException {
+        addressRepository.findById(addressId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Address not found with id : " + addressId));
+        addressService.delete(addressId);
+        return new ResponseEntity<>("주소지 삭제에 성공했습니다.", HttpStatus.OK);
     }
 
 }
